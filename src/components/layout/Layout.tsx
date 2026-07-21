e22c078176636bb0d263168004f106ff1603808f
matin mobini <virtualmatinvmatin@gmail.com>
2026-07-21 15:39:53 +0330

    changing the somr part of navbar and making a component named container and making a layout for app.tsx to put routes in it  as children and move navbar component in the layout component and put in head of children

---
+++ src/components/layout/Layout.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Navbar from '../navbar/Navbar'
+
+interface Ilayout {
+    children : React.ReactNode
+}
+
+function Layout( {children  } : Ilayout ) {
+  return (
+    <>
+    <Navbar />
+    {children }
+    
+    </>
+  )
+}
+
+export default Layout
