6c9e59eb831697c0029359b0a928235853d01637
matin mobini <virtualmatinvmatin@gmail.com>
2026-07-28 14:50:08 +0330

    creating the first version of productPage

--- src/components/productitem/ProductItem.tsx
+++ src/components/productitem/ProductItem.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 function ProductItem() {
   return (
+
     <div className="group bg-white rounded-2xl shadow-sm hover:shadow-xl border border-amber-50 overflow-hidden transition-all duration-300 hover:-translate-y-1">
       
       {/* تصویر */}
@@ -31,12 +32,12 @@ function ProductItem() {
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste eaque sequi nemo nostrum eos veritatis quaerat veniam, inventore ex pariatur, nihil modi assumenda perferendis architecto vero? Debitis voluptatum voluptates delectus?
         </p>
 
-        <button className="w-full flex items-center justify-center gap-2 bg-amber-500 hover:bg-amber-600 active:scale-95 text-white font-medium py-2.5 rounded-xl transition-all duration-200">
+        {/* <button className="w-full flex items-center justify-center gap-2 bg-amber-500 hover:bg-amber-600 active:scale-95 text-white font-medium py-2.5 rounded-xl transition-all duration-200">
           <svg xmlns="http://www.w3.org/2000/svg" className="w-4 h-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
             <path strokeLinecap="round" strokeLinejoin="round" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
           </svg>
           افزودن به سبد خرید
-        </button>
+        </button> */}
       </div>
     </div>
   )
