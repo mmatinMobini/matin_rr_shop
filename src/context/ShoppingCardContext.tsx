6b33e415dc22a3abad2c5786947d233bc573d4cc
matin mobini <virtualmatinvmatin@gmail.com>
2026-08-25 15:44:02 +0330

    completing some issues in context and make it smoother

--- src/context/ShoppingCardContext.tsx
+++ src/context/ShoppingCardContext.tsx
@@ -1,22 +1,42 @@
-import React, {  createContext, useState } from "react";
+import React, {  createContext, useContext, useState } from "react";
 
 
     interface IShoppingCardContext_Provider {
         children  :  React.ReactNode
     }
 
+    
+    
+    export   interface ICardItem {
+        id : number ,
+        qty :number
+
+    }
 
  export   interface  IShoppingCardContext_itSelf {
-                cardItem  : []
+                cardItem  : ICardItem[]
     }
-export const ShoppingCardContext = createContext<IShoppingCardContext_itSelf>({
-    cardItem : []
-})
+
+ 
+    // cart  =[
+    //     {id : 1 , qty : 2 }, {id : 4 , qty : 5 }
+    // ]
+
+
+
+
+export const ShoppingCardContext = createContext({} as IShoppingCardContext_itSelf  );
+
+//mixing useContext(ShoppingCardContext) with custom hook 
+    export const useShoppingCardContext =()=>{
+
+            return useContext(ShoppingCardContext)
+    }
 
 
 export function ShoppingcardContext_Provider(  {  children  } : IShoppingCardContext_Provider ){
 
-    const [cardItem, setcardItem ] = useState<[]>([])
+    const [cardItem, setcardItem ] = useState<ICardItem[]>([])
 
     return(
 
